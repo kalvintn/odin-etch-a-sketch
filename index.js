b658494d3beaef7e77d7b5b751ae2598adbe37a2
kalvintn <63025530+kalvintn@users.noreply.github.com>
2025-11-07 23:48:14 -0500

    Button functionality - change size.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 
 let grid = document.querySelector(".grid");
+let sizeButton = document.querySelector(".size");
 let gridWidth = 600;
 let gridHeight = 600;
 
-
+// default grid size
 createGrid(16);
 
-
+sizeButton.addEventListener("click", promptSize);
 
 
 
 
 function createGrid(size){
+    // Clear grid
+    grid.innerHTML = "";
+
     // Create a grid of div (size x size) dimensions
     for(let i = 0; i < size * size; i++){
             let newSquare = document.createElement("div");
@@ -25,4 +29,16 @@ function createGrid(size){
     }
 
     console.log(grid);
+}
+
+
+// Custom grid size from user prompt
+function promptSize(){
+    let newSize = prompt("Please enter a grid size (from 2 to 100): ");
+
+    while(!(newSize >= 2 && newSize <= 100)){
+        newSize = prompt("Try again. Please enter a grid size (from 2 to 100):");
+    }
+
+    createGrid(newSize);
 }
