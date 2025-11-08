60ff483ff8224641a67970f515aba1e29e6e8a4f
kalvintn <63025530+kalvintn@users.noreply.github.com>
2025-11-07 22:11:26 -0500

    Loop to create square div grid.

---
+++ index.js
@@ -0,0 +1,33 @@
+
+let grid = document.querySelector(".grid");
+let gridWidth = 600;
+let gridHeight = 600;
+
+
+createGrid(16);
+
+
+
+
+
+
+function createGrid(size){
+    // Create a grid of div (size x size) dimensions
+    for(let i = 0; i < size - 1; i++){
+        for(let j = 0; j < size; j++){
+            let newSquare = document.createElement("div");
+
+            newSquare.style.width = (gridWidth / size) + "px";
+            newSquare.style.height = newSquare.style.width;
+            newSquare.style.border = "1px solid black";
+
+
+            newSquare.style.border.borderCollapse = "collapse";
+            newSquare.style.boxSizing = "border-box";
+            
+            grid.appendChild(newSquare);
+        }
+    }
+
+    console.log(grid);
+}
